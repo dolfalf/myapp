7fe7712ad9d5054287f6fb2f8b6ded50ed71d579
vagrant <vagrant@localhost.localdomain>
2018-02-24 18:26:32 +0900

    first commit

---
+++ db/migrate/20180224092250_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.datetime :post_date
+      t.boolean :is_public
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
